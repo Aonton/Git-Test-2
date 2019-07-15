b757c70c1388f349724d651db1f3e21bdc63aeec
Amy Feng <afeng5@fordham.edu>
2019-07-15 09:39:08 -0400

    Initial Commit

---
+++ Git Test 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Test 2
+//
+//  Created by Amy Feng on 7/15/19.
+//  Copyright © 2019 Amy Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
